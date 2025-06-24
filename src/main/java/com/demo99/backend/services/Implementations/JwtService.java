b55b01b6431485801e1df68c126219971f3626dd
Imad Iyad <emadeyad063@gmail.com>
2025-06-24 11:32:16 +0300

    update JWT

--- src/main/java/com/demo99/backend/services/Implementations/JwtService.java
+++ src/main/java/com/demo99/backend/services/Implementations/JwtService.java
@@ -2,34 +2,37 @@ package com.demo99.backend.services.Implementations;
 
 import com.demo99.backend.model.entities.User;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.security.Key;
 import java.util.Date;
+
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "your_super_long_secret_key_that_is_at_least_32_chars!";
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
     public String generateToken(User user) {
         return Jwts.builder()
-                .setSubject(user.getEmail())
+                .subject(user.getEmail())
                 .claim("role", user.getRole().name())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 يوم
-                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(key) // ✅ بدون algorithm
                 .compact();
     }
 
     public String extractUsername(String token) {
         return Jwts.parser()
-                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes())) // ✅ مهم جدًا
+                .verifyWith((SecretKey) key)
                 .build()
-                .parseClaimsJws(token)
-                .getBody()
+                .parseSignedClaims(token)
+                .getPayload()
                 .getSubject();
     }
 
