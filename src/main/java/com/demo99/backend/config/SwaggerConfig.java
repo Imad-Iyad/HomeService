99bb5ce788ae5162e13d4ebc4db42ded2c997f75
Imad Iyad <emadeyad063@gmail.com>
2025-07-11 18:00:40 +0300

    try fix the bug

--- src/main/java/com/demo99/backend/config/SwaggerConfig.java
+++ src/main/java/com/demo99/backend/config/SwaggerConfig.java
@@ -1,26 +1,35 @@
 package com.demo99.backend.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
+@OpenAPIDefinition(
+        info = @Info(title = "HomeService API", version = "1.0"),
+        servers = {
+                @Server(url = "https://homeservice-production-5b76.up.railway.app") // 👈 بيجبر Swagger يستخدم HTTPS
+        }
+)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "bearer"
 )
+@Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
+                .info(new io.swagger.v3.oas.models.info.Info()
                         .title("Demo API")
                         .version("1.0")
                 )
